283cde1430a1daf055c7c5715408ef306e31c152
Emmanuel <emmanuel.batista1@marist.edu>
2020-02-03 17:59:18 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,8 @@
+print("Weclome to Emmanuel's MadLibs!")
+noun = input("Please enter your noun:")
+verb = input("Please enter your verb:")
+adjective = input("Please enter your adjective:")
+place = input("Please enter your place:")
+print("Ladies and gentlemen, grab your",adjective,noun,"and get ready to",verb,"because this is",place+"!")
+
+
