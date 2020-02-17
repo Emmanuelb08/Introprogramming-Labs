9d6d14c3e063f5c9e4cad1b0c5437b67e07303dd
Emmanuel <emmanuel.batista1@marist.edu>
2020-02-17 18:27:47 -0500

    final version

--- guessing-game.py
+++ guessing-game.py
@@ -7,12 +7,17 @@ def main():
         print("I am thinking of an animal...")
         r = str(input("Can you guess what it is?\n"))
         r = r.lower()
-        if a==r:
-            x=1
-            print("Congragulations! You've guessed it.")
-        elif r==q:
+        if r[0] == "q":
             x=1
             print("No one likes a quitter. Come back soon!")
+        elif a==r:
+            x=1
+            print("Congragulations! You've guessed it.")
+            like=input("Do you like {}'s?".format(a))
+            if like[0] == "y":
+                print("They're just the coolest, aren't they!")
+            elif like[0] == "n":
+                print("Why not? They're awesome!?")
         else:
             print("Sorry, that is incorrect. You can type quit to give up. Please try again!")
 
