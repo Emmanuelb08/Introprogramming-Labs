c558bb46b2abd68fd916f0b9e8d43db2ec8f7c86
Emmanuel <emmanuel.batista1@marist.edu>
2020-02-17 17:59:36 -0500

    Usability improvements

--- guessing-game.py
+++ guessing-game.py
@@ -2,17 +2,23 @@ def main():
     print("Welcome to Emmanuel's Guessing Game!")
     a = "giraffe"
     x=0
+    q = "quit"
     while x == 0:
         print("I am thinking of an animal...")
         r = str(input("Can you guess what it is?\n"))
+        r = r.lower()
         if a==r:
             x=1
             print("Congragulations! You've guessed it.")
+        elif r==q:
+            x=1
+            print("No one likes a quitter. Come back soon!")
         else:
-            print("Sorry, that is incorrect. Please try again!")
+            print("Sorry, that is incorrect. You can type quit to give up. Please try again!")
 
 main()
                 
         
         
         
+
